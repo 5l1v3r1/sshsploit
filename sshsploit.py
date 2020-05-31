312a99ae262275a2a3cd3f9f4474294c8caf7881
Ivan Nikolsky <54115104+enty8080@users.noreply.github.com>
2020-05-31 12:41:29 +0300

    Update sshsploit.py

--- sshsploit.py
+++ sshsploit.py
@@ -1,1 +1,30 @@
+#!/usr/bin/env python3
 
+import os
+
+os.system("printf '\033]2;Ghost Framework\a'")
+
+import sys
+import subprocess
+import readline
+import time
+
+Q = '\033[1;77m[?] \033[0m'
+G = '\033[1;34m[*] \033[0m'
+S = '\033[1;32m[+] \033[0m'
+W = '\033[1;33m[!] \033[0m'
+E = '\033[1;31m[-] \033[0m'
+
+readline.parse_and_bind("tab: complete")
+
+def banner():
+    os.system("clear")
+    os.system("cat banner/banner.txt")
+    print("")
+    print("SSHSploit Framework v1.0")
+    print("------------------------")
+    print("")
+
+def main():
+    while True:
+        break
